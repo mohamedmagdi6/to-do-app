be0977819498d6ce7e2c402bd33f9eb006fba811
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-08-10 11:47:30 +0300

    integration with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_app/firebase_options.dart';
 import 'package:to_do_app/screens/edit_task_screen.dart';
 import 'package:to_do_app/screens/home_screen.dart';
 import 'package:to_do_app/screens/splash_screen.dart';
 import 'package:to_do_app/theme_data.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
