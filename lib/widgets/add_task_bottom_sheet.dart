cbeed146cc69cde5848bb9f92e1e4ce5e6f305c6
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-08-11 12:35:28 +0300

    adding custom button to bottomSheet and doing datePicker

--- lib/widgets/add_task_bottom_sheet.dart
+++ lib/widgets/add_task_bottom_sheet.dart
@@ -1,58 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:to_do_app/constants/color_constant.dart';
+import 'package:to_do_app/widgets/custom_button.dart';
 import 'package:to_do_app/widgets/custom_text_field.dart';
 
-class AddTaskBootomSheet extends StatelessWidget {
+class AddTaskBootomSheet extends StatefulWidget {
   const AddTaskBootomSheet({super.key});
 
+  @override
+  State<AddTaskBootomSheet> createState() => _AddTaskBootomSheetState();
+}
+
+class _AddTaskBootomSheetState extends State<AddTaskBootomSheet> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController subTitleController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  DateTime selectionDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 32, left: 44, right: 44),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            'Add new Task',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w700,
-              fontSize: 18,
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add new Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w700,
+                fontSize: 18,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            hintText: 'enter your task',
-            color: Color(0xFFA9A9A9),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Select time',
-            style: TextStyle(
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
+            SizedBox(
+              height: 20,
             ),
-          ),
-          Text(
-            '12:00 Am',
-            textAlign: TextAlign.center,
-            style: TextStyle(
+            CustomTextField(
+              controller: titleController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+              hintText: 'enter your task',
+              color: Color(0xFFA9A9A9),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              controller: subTitleController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+              hintText: 'Description',
+              color: Color(0xFFA9A9A9),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Select time',
+              style: TextStyle(
                 fontFamily: 'Inter',
                 fontWeight: FontWeight.w400,
                 fontSize: 20,
-                color: Color(0xFFA9A9A9)),
-          ),
-          SizedBox(
-            height: 30,
-          )
-        ],
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                DateTime? chosenDate = await showDatePicker(
+                  context: context,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime.now().add(
+                    Duration(days: 365),
+                  ),
+                  initialDate: selectionDate,
+                  builder: (context, child) {
+                    return Theme(
+                        data: ThemeData.light().copyWith(
+                            colorScheme: ColorScheme.light(
+                          primary: primaryColor,
+                        )),
+                        child: child!);
+                  },
+                );
+                if (chosenDate != null) {
+                  selectionDate = chosenDate;
+                  setState(() {});
+                }
+              },
+              child: Text(
+                selectionDate.toString().substring(0, 10),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                    color: Color(0xFFA9A9A9)),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            customButton(
+              buttonName: 'Add TAsk',
+              color: primaryColor,
+              onPressed: () {
+                if (formKey.currentState!.validate()) {}
+              },
+            ),
+            SizedBox(
+              height: 30,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class CustomTextField extends StatelessWidget {
-  CustomTextField({required this.hintText, this.color});
+  CustomTextField(
+      {required this.hintText, this.color, this.validator, this.controller});
   String hintText;
   Color? color;
+  final String? Function(String?)? validator;
+  TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      controller: controller,
+      validator: validator,
       decoration: InputDecoration(
         focusedBorder: UnderlineInputBorder(
             borderSide: BorderSide(
